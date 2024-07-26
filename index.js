22e8da61484344f884a44d239a12be5b7f23269c
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-26 13:41:14 +0300

    creates class evergreen

--- index.js
+++ index.js
@@ -21,3 +21,15 @@ class  Deciduous extends Tree {
       super.definition() + " Deciduous trees shed their leaves annually." )
   }
 }
+
+class Evergreen extends Tree {
+  constructor (species, name){
+    super(species)
+    this.name = name;
+  }
+
+  static definition(){
+    return (
+    super.definition() + " Evergreens keep their leaves all year round.")
+  }
+}
