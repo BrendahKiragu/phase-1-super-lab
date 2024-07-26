80a765f8c76855b7b18f404054571213541063e4
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-26 13:25:47 +0300

    creates class decidous that inherits parent class Tree

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ class Tree {
 }
 //console.log(Tree.definition)
 
+class  Deciduous extends Tree {
+  constructor (species, name){
+    super(species)
+    this.name = name;
+  }
+
+  static definition(){
+    return (
+      super.definition() + " Deciduous trees shed their leaves annually." )
+  }
+}
